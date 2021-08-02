2c9e62cc316cdd90096688b158b838e2453863f3
delphic <delphic.uk@gmail.com>
2021-08-02 23:58:55 +0100

    Updated fury submodule

--- libs/fury.js
+++ libs/fury.js
@@ -482,16 +482,17 @@ var Fury = module.exports = (function() {
 	};
 
 	Fury.init = function(parameters) {
-		let lightWeightInit = false;
+		let disableShaderPreload = false;
 		let canvasId = null;
 		let contextAttributes = null;
 
 		if (typeof(parameters) == 'string') {
-			lightWeightInit = true;
+			disableShaderPreload = true;
 			canvasId = parameters;
 		} else {
 			canvasId = parameters.canvasId;
 			contextAttributes = parameters.glContextAttributes;
+			disableShaderPreload = !!parameters.disableShaderPreload;
 		}
 
 		canvas = document.getElementById(canvasId);
@@ -503,12 +504,14 @@ var Fury = module.exports = (function() {
 		}
 		Fury.Input.init(canvas);
 
-		if (!lightWeightInit) {
+		if (!disableShaderPreload) {
 			Fury.Shaders.createShaders();
-			if (parameters.gameLoop) {
-				Fury.GameLoop.init(parameters.gameLoop);
-			}
 		}
+		
+		if (parameters.gameLoop) {
+			Fury.GameLoop.init(parameters.gameLoop);
+		}
+
 		return true;
 	};
 
