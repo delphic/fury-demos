3783d217278d7412a3afe75e83044b5391b87587
Harry Jones <delphic.uk@gmail.com>
2024-12-07 11:12:34 +0000

    Update Fury Submodule

--- libs/fury.js
+++ libs/fury.js
@@ -775,6 +775,12 @@ module.exports = (function(){
 	// also want references that do not go out of date when 
 	// you remove an item (which is hopefully rarely).
 
+	// Can this be replaced with Map?
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+	// An issue is the lack of a sort functionw without creating a new map, which is used 
+	// when adding new objects to sceneObjects (in scene.add in the scene module) to group objects
+	// by their material.
+
 	// Please note, again for purposes of speed and ease of use 
 	// this structure adds the key of the item to the id property on items added
 	// this eases checking for duplicates and if you have the only reference
@@ -7953,6 +7959,48 @@ exports.scale = function(out, a, b) {
 };
 
 /**
+ * Scales a vec3 by a scale number in X and Y axes only
+ * @param {vec3} out the receiving vector 
+ * @param {ReadonlyVec3} a the vector to scale 
+ * @param {Number} b amount to scale the vector by 
+ * @returns {vec3} out 
+ */
+exports.scaleXY = function(out, a, b) {
+	out[0] = a[0] * b;
+	out[1] = a[1] * b;
+	out[2] = a[2];
+	return out;
+};
+
+/**
+ * Scales a vec3 by a scale number in X and Z axes only
+ * @param {vec3} out the receiving vector 
+ * @param {ReadonlyVec3} a the vector to scale 
+ * @param {Number} b amount to scale the vector by 
+ * @returns {vec3} out 
+ */
+exports.scaleXZ = function(out, a, b) {
+	out[0] = a[0] * b;
+	out[1] = a[1];
+	out[2] = a[2] * b;
+	return out;
+};
+
+/**
+ * Scales a vec3 by a scale number in Y and Z axes only
+ * @param {vec3} out the receiving vector 
+ * @param {ReadonlyVec3} a the vector to scale 
+ * @param {Number} b amount to scale the vector by 
+ * @returns {vec3} out 
+ */
+exports.scaleYZ = function(out, a, b) {
+	out[0] = a[0];
+	out[1] = a[1] * b;
+	out[2] = a[2] * b;
+	return out;
+};
+
+/**
  * Adds two vec3's after scaling the second operand by a scalar value
  *
  * @param {vec3} out the receiving vector
@@ -9377,6 +9425,9 @@ module.exports = (function(){
 	};
 
 	let createBuffer = function(data, size, indexed) {
+		if (data.length > 65535) {
+			console.warn("buffer contains more than 16-bit max number of points, rendering may be truncated");
+		}
 		if (data.buffer) {
 			if (!indexed) {
 				return r.createArrayBuffer(data, size);
@@ -10056,6 +10107,8 @@ module.exports = (function(){
 		let exports = {};
 		let prototype = {};
 
+		let cacheVec3 = vec3.create();
+
 		exports.contains = function(point, sphere) {
 			let dx = point[0] - sphere.center[0], dy = point[1] - sphere.center[1], dz = point[2] - sphere.center[2];
 			let sqrDistance = dx * dx + dy * dy + dz * dz;
@@ -10070,6 +10123,47 @@ module.exports = (function(){
 		
 		exports.intersectBox = function(box, sphere) {
 			return Box.intersectSphere(sphere, box);
+		};
+
+		// out is vec3 of intersection point, returns intersection time or 0 if no intersection
+		exports.rayCast = function(out, origin, direction, sphere) {
+			// Ray = origin + time * direction
+			// (intersection - sphere.center) . (intersection - sphere.center) = sphere.radius * sphere.radius
+			// => (origin - sphere.center + time * direction) . (origin - sphere.center + time * direction) = spehere.radius * sphere.radius
+			// Let m = origin - sphere.center, t = time, d = direction and r = sphere.radius
+			// simplifies to: (m + td) . (m + td) = r*r
+			// leading to quadratic equation: t*t + 2(m.d)t + (m.m) - r*r = 0
+			// quadric formula: t = -b +/- sqrt(b*b - c)
+			// b = m.d, c = m.m - r*r
+			// discriminant (b*b - c): < 0 => no intersection, 0 => one root, > 0 implies 2 roots
+			let m = cacheVec3;
+			vec3.subtract(m, origin, sphere.center);
+			let b = vec3.dot(m, direction);
+			let c = vec3.dot(m, m) - sphere.radius * sphere.radius;
+	
+			if (c > 0 && b > 0) {
+				// origin outside sphere (c > 0) & direction points away from sphere (b > 0)
+				// No intersection
+				return 0;
+			}
+			if (c < 0) {
+				// origin inside sphere, which we exclude by convention
+				return 0;
+			}
+			
+			let discriminant = b * b - c;
+			if (discriminant < 0) {
+				// No real roots
+				return 0;
+			}
+			
+			let t = -b - Math.sqrt(discriminant);
+			if (t < 0) {
+				// Note shouldn't happen (implies inside the sphere, which we have excluded)
+				t = 0;
+			}
+			vec3.scaleAndAdd(out, origin, direction, t);
+			return t;
 		};
 		
 		exports.create = function({ center = vec3.create(), radius = 0 }) {
