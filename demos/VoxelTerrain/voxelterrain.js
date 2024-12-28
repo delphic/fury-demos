a65e20ae4346f30db3273c81bed9a4a30dd43519
delphic <delphic.uk@gmail.com>
2024-12-28 15:09:29 +0000

    Use Fury WorkerPool

--- demos/VoxelTerrain/voxelterrain.js
+++ demos/VoxelTerrain/voxelterrain.js
@@ -243,52 +243,8 @@ let awake = function() {
 	loop();
 };
 
-let WorkerPool = (function() {
-	let exports = {};
-
-	let prototype = {
-		maxWorkers: 8,
-		isWorkerAvailable: function() {
-			return this.inUseWorkerCount < this.maxWorkers;
-		},
-		requestWorker: function() {
-			if (this.workerSrc) {
-				for (let i = 0; i < this.maxWorkers; i++) {
-					if (!this.workerInUse[i]) {
-						if (!this.workers[i]) {
-							this.workers[i] = new Worker(this.workerSrc);
-							this.workers[i].workerIndex = i;
-						}
-						this.workerInUse[i] = true;
-						this.inUseWorkerCount++;
-						return this.workers[i];
-					}
-				}
-			}
-			return null;
-		},
-		returnWorker: function(worker) {
-			this.workerInUse[worker.workerIndex] = false;
-			this.inUseWorkerCount--;
-		}
-	};
-
-	exports.create = function(config) {
-		let pool = Object.create(prototype);
-		pool.workerSrc = config.src;
-		if (config.maxWorkers) pool.maxWorkers = config.maxWorkers;
-		pool.inUseWorkerCount = 0;
-		pool.workers = [];
-		pool.workerInUse = [];
-
-		return pool;
-	};
-
-	return exports;
-})();
-
-let generatorPool = WorkerPool.create({ src: 'generator.js', maxWorkers: 8 });
-let mesherPool =  WorkerPool.create({ src: 'mesher.js', maxWorkers: 4 });
+let generatorPool = Fury.WorkerPool.create({ src: 'generator.js', maxWorkers: 8 });
+let mesherPool =  Fury.WorkerPool.create({ src: 'mesher.js', maxWorkers: 4 });
 
 let vorld = Vorld.create({ chunkSize: 16 }); // Amalgamated Vorld Data
 
